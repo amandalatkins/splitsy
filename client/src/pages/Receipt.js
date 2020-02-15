0f5e865c4131b7f62399c84c89a46f1943f1ea40
Amanda Atkins <amanda@digitalrainstorm.com>
2020-02-15 13:23:10 -0800

    Built functionality to add an item to a receipt

--- client/src/pages/Receipt.js
+++ client/src/pages/Receipt.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useReceiptContext } from "../utils/ReceiptState";
 import API from "../utils/API";
 import PayersList from "../components/PayersList";
@@ -10,6 +10,11 @@ function Receipt(props) {
 
     const [receiptState, receiptStateDispatch] = useReceiptContext();
     const receiptId = props.match.params.id;
+
+    const [addItem, setAddItem] = useState(false);
+
+    const itemName = useRef();
+    const itemPrice = useRef();
 
     useEffect(() => {
         loadReceipt(receiptId);
@@ -27,6 +32,21 @@ function Receipt(props) {
 
     function toggleEditState() {
         receiptStateDispatch({ type: "toggleEditState" });
+    }
+
+    function toggleAddItem() {
+        setAddItem(!addItem);
+    }
+
+    function addNewItem() {
+        API.createItem({ ReceiptId: receiptId, name: itemName.current.value, price: itemPrice.current.value })
+        .then(_ => {
+            loadReceipt(receiptId)
+            itemName.current.value = "";
+            itemPrice.current.value = "";
+            toggleAddItem();
+        })
+        .catch(err => console.log(err));
     }
 
     return (
@@ -72,6 +92,24 @@ function Receipt(props) {
                                             return <ReceiptItemEdit key={item.id} item={item} isTotalItem={false} loadReceipt={loadReceipt} />
                                         }) : ""}
 
+                                        {addItem ?
+                                            <tr className="receipt-item">
+                                                <td className="receipt-item-label text-left">
+                                                    <input className="form-control" type="text" ref={itemName}/>
+                                                    <p className="mt-2">
+                                                        <button className="btn btn-secondary btn-sm mr-1" onClick={() => addNewItem()}>Save</button>
+                                                    </p>
+                                                </td>
+                                                <td className="receipt-item-price text-right"><input className="form-control" type="text" ref={itemPrice}/></td>
+
+                                            </tr>
+                                        : "" }
+
+                                        <tr className="receipt-item">
+                                            <td className="receipt-item-label text-left"><button className="btn btn-sm btn-secondary" onClick={toggleAddItem}>Add Item</button></td>
+                                            <td className="receipt-item-price text-right"></td>
+                                        </tr>
+
                                         {receiptState.receipts.length ? 
                                             <ReceiptItemEdit 
                                                 item={{ name: "Subtotal",  price: receiptState.receipts[0].total, receiptId: receiptState.receipts[0].id }}
@@ -108,11 +146,6 @@ function Receipt(props) {
                                         {receiptState.receipts.length ? receiptState.receipts[0].Items.map(item => {
                                             return <ReceiptItem key={item.id} item={item} isTotalItem={false}  />
                                         }) : ""}
-
-                                        <tr className="receipt-item">
-                                            <td className="receipt-item-label text-left"><button className="btn btn-sm btn-secondary">Add Item</button></td>
-                                            <td className="receipt-item-price text-right"></td>
-                                        </tr>
                                         
                                         {receiptState.receipts.length ? 
                                             <ReceiptItem 
