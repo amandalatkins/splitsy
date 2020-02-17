6c50ff67b399717f2ab473b4a8e4f605f2575bba
Arman Riahi <namrataffy@gmail.com>
2020-02-17 12:20:06 -0800

    pulled from master

--- client/src/utils/ReceiptState.js
+++ client/src/utils/ReceiptState.js
@@ -4,42 +4,42 @@ const ReceiptContext = createContext();
 const { Provider } = ReceiptContext;
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "loadReceipts":
-          return {
-              ...state,
-              receipts: action.receipts
-          }
-        case "toggleEditState":
-            if (!state.isEditMode) {
-                return {
-                    ...state,
-                    isEditMode: false
-                }
-            } else {
-                return {
-                    ...state,
-                    isEditMode: !state.isEditMode
-                }
-            }
-        default:
-          throw new Error(`Invalid action type: ${action.type}`);
-        }
-}
+  switch (action.type) {
+    case "loadReceipts":
+      return {
+        ...state,
+        receipts: action.receipts
+      };
+    case "toggleEditState":
+      if (!state.isEditMode) {
+        return {
+          ...state,
+          isEditMode: false
+        };
+      } else {
+        return {
+          ...state,
+          isEditMode: !state.isEditMode
+        };
+      }
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+};
 
 const defaultState = {
-    isEditMode: false,
-    currentPayer: null,
-    receipts: []
-}
+  isEditMode: false,
+  currentPayer: null,
+  receipts: []
+};
 
-const ReceiptProvider = ({ value = defaultState , ...props }) => {
-    const [state, dispatch] = useReducer(reducer, defaultState);
-    return <Provider value={[state, dispatch]} {...props} />
-}
+const ReceiptProvider = ({ value = defaultState, ...props }) => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
 
 const useReceiptContext = () => {
-    return useContext(ReceiptContext);
-}
+  return useContext(ReceiptContext);
+};
 
 export { ReceiptProvider, useReceiptContext };
